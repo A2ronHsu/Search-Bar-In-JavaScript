545a481c41623fb05436ec0c4a3d2c39e6f90ac6
Aaron <aaronbee1990@gmail.com>
2025-02-26 12:42:15 -0300

    finished filtering logic for search bar; finished the project

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
 const userCardTemplate = document.querySelector("[data-user-template]");
 const userCardContainer = document.querySelector("[data-user-cards-container]");
-const searchInput =
+const searchInput = document.querySelector("[data-search-input]");
+
+let documentFragment = document.createDocumentFragment();
+const cards = userCardContainer.children;
+console.log(cards)
+
+searchInput.addEventListener("input", e => {
+   const value = e.target.value.toLowerCase();
+
+   for (const card of cards) {
+      const header = card.querySelector("[data-header]");
+      const body = card.querySelector("[data-body]");
+      const isVisible =
+         header.textContent.toLowerCase().includes(value) ||
+         body.textContent.toLowerCase().includes(value);
+
+      console.log(header.textContent, body.textContent, isVisible,card.classList);
+      card.classList.toggle("hide", !isVisible);
+      
+   }
+})
+
+
 
 fetch("https://jsonplaceholder.typicode.com/users")
    .then(res => res.json())
-   .then(data => { 
-      let documentFragment = document.createDocumentFragment();
-      data.forEach(user =>{
+   .then(data => {
+      users = data.map(user => {
          const card = userCardTemplate.content.cloneNode(true).children[0];
          const header = card.querySelector("[data-header]");
          const body = card.querySelector("[data-body]");
          header.textContent = user.name;
          body.textContent = user.email;
          documentFragment.append(card);
-         console.log(user)
-         console.log(documentFragment)
+         return {
+            user: user.name,
+            email: user.email,
+            element: card
+         }
+         // console.log(user)
+         // console.log(documentFragment)
       });
       userCardContainer.append(documentFragment);
 
